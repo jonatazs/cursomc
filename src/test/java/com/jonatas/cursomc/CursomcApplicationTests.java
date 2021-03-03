23e615f0bd96b9266c34abf505a1480a4a7fbc17
Jonatas Oliveira <j.olivasc@outlook.com>
2021-03-03 12:40:47 -0300

    Project Created

---
+++ src/test/java/com/jonatas/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatas.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
